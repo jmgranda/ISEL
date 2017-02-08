83cc8d000268475bd518c8170270b180f9a06e53
jmgranda <jmgranda@floyd.lsi.die>
2017-02-08 13:34:21 +0100

    main.c

--- main.c
+++ main.c
@@ -21,24 +21,24 @@ extern int timer;
 
 
 // Utility functions, should be elsewhere
-struct timespec now, next_activation, timeout;
 
 // Wait until next_activation (clock_gettime and nanosleep)
 void delay_until (struct timespec* next_activation)
 {
   // struct timeval now, timeout;
+  struct timespec now, timeout;
   // gettimeofday (&now, NULL);
   clock_gettime (CLOCK_MONOTONIC, &now);
   timespec_sub (&timeout, next_activation, &now);
   // select (0, NULL, NULL, NULL, &timeout);
-  nanosleep (&now, NULL);
+  nanosleep (&timeout, NULL);
 }
 
 
 /* Coffee machine with credit check and give change activation */
 void* main_ec (void* arg)
 {
-  struct timespec clk_period = { 0, 250 * 1000 };
+  struct timespec clk_period = { 0, 250 * 1000000L };
   struct timespec next_activation;
 
   fsm_t* cofm_fsm = cofm_fsm_new ();
