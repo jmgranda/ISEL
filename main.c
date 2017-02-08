66dedd74e33a775aaaca0ea908b3d771c4d638b5
jmgranda <jmgranda@floyd.lsi.die>
2017-02-08 19:10:02 +0100

    main.c

--- main.c
+++ main.c
@@ -21,16 +21,13 @@ extern int timer;
 
 
 // Utility functions, should be elsewhere
-struct timespec now, next_activation, timeout;
 
 // Wait until next_activation (clock_gettime and nanosleep)
 void delay_until (struct timespec* next_activation)
 {
-  // struct timeval now, timeout;
-  // gettimeofday (&now, NULL);
+  struct timespec now, timeout;
   clock_gettime (CLOCK_MONOTONIC, &now);
   timespec_sub (&timeout, next_activation, &now);
-  // select (0, NULL, NULL, NULL, &timeout);
   nanosleep (&timeout, NULL);
 }
 
@@ -43,7 +40,6 @@ void* cofm_func (void* arg)
 
   fsm_t* cofm_fsm = cofm_fsm_new ();
 
-  // gettimeofday (&next_activation, NULL);
   clock_gettime (CLOCK_MONOTONIC, &next_activation);
   while (1) {
     fsm_fire (cofm_fsm);
@@ -60,7 +56,6 @@ void* purse_func (void* arg)
 
   fsm_t* purse_fsm = purse_fsm_new ();
 
-  // gettimeofday (&next_activation, NULL);
   clock_gettime (CLOCK_MONOTONIC, &next_activation);
   while (1) {
     fsm_fire (purse_fsm);
@@ -79,10 +74,7 @@ int main ()
   task_new ("cofm",  cofm_func,  250, 250, 2, 1024);
   task_new ("purse", purse_func, 250, 250, 1, 1024);
   interp_run ();
-  /*	while (scanf("%d %d %d %d", &button, &coin, &change, &timer) == 4)
-	timeval_add (&next_activation, &next_activation, &clk_period);
-	delay_until (&next_activation);
-  */
+
   return 0;
 }
 
